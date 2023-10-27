c27c5de35d9ffa94cf61c0ad30302e64aa44b635
Sean McKay <seanryanmckay@gmail.com>
2023-10-27 16:41:59 +0900

    install and setup jest with one unit test (#67)
    * test: install and setup jest with one unit test
    
    config: change rootDir in tsconfig to support typescript in root
    
    config: add jest.config file
    
    config: create singleton file
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>
    
    test: write unit test for createUser in signup model
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>
    
    fix: correct spelling in signup.test
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>
    
    config: add test script to package.json
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>
    
    * Update README.md
    
    ---------
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>

---
+++ __tests__/signup.test.ts
@@ -0,0 +1,45 @@
+import signupModel from '../src/model/signup.model';
+import { prismaMock } from '../singleton';
+
+const japanTime = new Date().toLocaleString('en-US', {
+  timeZone: 'Asia/Tokyo',
+});
+
+interface User {
+  id: number;
+  username: string;
+  email: string;
+  uid: string;
+  created_at: string;
+  updated_at: string;
+}
+
+describe('createUser', () => {
+  it('Should create a new user', async () => {
+    const user: User = {
+      id: 1,
+      username: 'frogman',
+      email: 'frogman@test.com',
+      uid: 'ba927d96-3b2d-11ee-be56-0242ac120002x',
+      created_at: japanTime,
+      updated_at: japanTime,
+    };
+
+    prismaMock.users.create.mockResolvedValue(user);
+
+    const newUser = await signupModel.createUser(
+      'frogman',
+      'frogman@test.com',
+      'ba927d96-3b2d-11ee-be256-0242ac120002x',
+    );
+
+    expect(newUser).toEqual({
+      id: 1,
+      username: 'frogman',
+      email: 'frogman@test.com',
+      uid: 'ba927d96-3b2d-11ee-be56-0242ac120002x',
+      created_at: japanTime,
+      updated_at: japanTime,
+    });
+  });
+});
