d59c7b15b56d6f2ce7580bff95182070520ead41
shonancanuck <shonanxspark@gmail.com>
2023-11-17 18:21:42 +0900

    fixup: convert logPrismaError to use switch cases

--- src/utils/prisma.error.ts
+++ src/utils/prisma.error.ts
@@ -1,38 +1,46 @@
 import { Prisma } from '@prisma/client';
 
 const logPrismaError = (e: unknown) => {
-  if (e instanceof Prisma.PrismaClientValidationError) {
-    console.log(
-      'Prisma client validation error. A field is missing or of the incorrect type.',
-    );
-  }
-
-  if (e instanceof Prisma.PrismaClientKnownRequestError) {
-    console.log('Prisma error code: ', e.code);
-    if (e.code === 'P2002') {
+  switch (true) {
+    case e instanceof Prisma.PrismaClientValidationError:
       console.log(
-        `There is a unique constraint violation, a new user cannot be created with this ${e.meta?.target}`,
+        'Prisma client validation error. A field is missing or of the incorrect type.',
       );
-    }
-  }
+      break;
 
-  if (e instanceof Prisma.PrismaClientUnknownRequestError) {
-    console.log(
-      'Prisma client unknown request error. Request has missing error code.',
-    );
-  }
+    case e instanceof Prisma.PrismaClientKnownRequestError:
+      const code = (<Prisma.PrismaClientKnownRequestError>e).code;
+      console.log('Prisma error code: ', code);
+      if (code === 'P2002') {
+        const target = (<Prisma.PrismaClientKnownRequestError>e).meta?.target;
+        console.log(
+          `There is a unique constraint violation, a new user cannot be created with this ${target}`,
+        );
+      }
+      break;
 
-  if (e instanceof Prisma.PrismaClientRustPanicError) {
-    console.log(
-      'Prisma client Rust panic error. A low-level database error occurred.',
-    );
-  }
+    case e instanceof Prisma.PrismaClientUnknownRequestError:
+      console.log(
+        'Prisma client unknown request error. Request has missing error code.',
+      );
+      break;
 
-  if (e instanceof Prisma.PrismaClientInitializationError) {
-    console.log(
-      'Prisma client initialization error. Could not connect to the database.',
-    );
-    console.log('Prisma error: ', e.errorCode);
+    case e instanceof Prisma.PrismaClientRustPanicError:
+      console.log(
+        'Prisma client Rust panic error. A low-level database error occurred.',
+      );
+      break;
+
+    case e instanceof Prisma.PrismaClientInitializationError:
+      const errorCode = (<Prisma.PrismaClientInitializationError>e).errorCode;
+      console.log(
+        'Prisma client initialization error. Could not connect to the database.',
+      );
+      console.log('Prisma error: ', errorCode);
+      break;
+
+    default:
+      console.log('An error occured attempting to access the database');
   }
 };
 
