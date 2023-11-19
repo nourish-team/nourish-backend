e405c859de32db7127eff400bd21016b4a6b2332
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-11-19 12:24:39 +0800

    Create additional validation checks for use in user creation
    feat: implement checks for empty inputs, valid uids, emails and usernames
    
    Signed-off-by: oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
    
    fix: Handle Prisma errors with custom messages
    
    Add try-catch block and async/await for improved error handling.
    
    fix: lint errors
    
    fix: improve user feedback by sending error msg

--- src/service/signup.service.ts
+++ src/service/signup.service.ts
@@ -7,21 +7,44 @@ interface SignupData {
   uid: string;
 }
 
+interface CustomError extends Error {
+  code?: string;
+  target?: string;
+}
+
 export default {
-  createUser(userdata: SignupData) {
+  async createUser(userdata: SignupData) {
     const {
       username,
       email,
       uid,
     }: { username: string; email: string; uid: string } = userdata;
 
-    // Validate username using the isSafeInput function
-    if (!validation.isSafeInput(username)) {
-      throw new Error(
-        'Invalid input. Please check your inputs.',
-      );
-    }
+    try {
+      // Validate username
+      if (!validation.isSafeInput(username) || !validation.isValidUsername(username)) {
+        throw new Error('Invalid username. Please check your input.');
+      }
 
-    return modelSignup.createUser(username, email, uid);
+      // Validate email
+      if (!validation.isNotEmptyInput(email) || !validation.isValidEmail(email)) {
+        throw new Error('Invalid email. Please check your input.');
+      }
+
+      // Validate UID
+      if (!validation.isNotEmptyInput(uid) || !validation.isValidUID(uid)) {
+        throw new Error('Something went wrong. Please check your inputs.');
+      }
+
+      return await modelSignup.createUser(username, email, uid);
+    } catch (error) {
+      const helper = error as CustomError;
+      // Prisma error
+      if (helper.code === 'P2002') {
+        throw new Error('User with this username or email already exists.');
+      }
+      // General error
+      throw error;
+    }
   },
 };
