53e067cef4bda2cf2c0d86af5c9d1b41a6e99817
Sean McKay <seanryanmckay@gmail.com>
2023-11-21 10:52:51 +0900

    Handle Prisma related errors in signup model (#76)
    * Add error handling for signup
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>
    
    enhancement: add function to log prima errors in signup model
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>
    
    style: remove comments from signup model
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>
    
    chore: move logPrismaError function to utils
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>
    
    enhance: edit readme to include information about error logs
    
    chore: remove about errors section from readme
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>
    
    refactor: remove else if blocks from logPrismaError in favour of if blocks
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>
    
    * fixup: convert logPrismaError to use switch cases
    
    ---------
    
    Co-authored-by: FefesG5 <fefesg5@users.noreply.github.com>

--- src/model/signup.model.ts
+++ src/model/signup.model.ts
@@ -1,7 +1,5 @@
-// import { PrismaClient} from '@prisma/client';
-// const prisma = new PrismaClient();
-
 import { prisma } from '../utils/db.server';
+import { logPrismaError } from '../utils/prisma.error';
 
 type User = {
   id: number;
@@ -16,19 +14,24 @@ export default {
     const japanTime = new Date().toLocaleString('en-US', {
       timeZone: 'Asia/Tokyo',
     });
-    const userInfo = await prisma.users.create({
-      data: {
-        username,
-        email,
-        uid,
-        updated_at: japanTime,
-        created_at: japanTime,
-      },
-      select: {
-        id: true,
-        username: true,
-      },
-    });
-    return userInfo;
+    try {
+      const userInfo = await prisma.users.create({
+        data: {
+          username,
+          email,
+          uid,
+          updated_at: japanTime,
+          created_at: japanTime,
+        },
+        select: {
+          id: true,
+          username: true,
+        },
+      });
+      return userInfo;
+    } catch (e) {
+      logPrismaError(e);
+      throw e;
+    }
   },
 };
