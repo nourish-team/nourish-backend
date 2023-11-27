33152a434ee6d8240bb9831cfba00ffdc8547bf2
shonancanuck <shonanxspark@gmail.com>
2023-11-27 15:25:47 +0900

    implement unit tests for signup
    refactor: remove error message in unit tests
    
    refactor: remove unused variable in signup unit tests
    
    test: implement unit test for getProductById in product model
    
    fix: rewrite createUser unit tests to avoid falsely passing tests
    
    fix: rewrite product unit tests to avoid falsely passing

--- __tests__/signup.test.ts
+++ __tests__/signup.test.ts
@@ -42,4 +42,52 @@ describe('createUser', () => {
       updated_at: japanTime,
     });
   });
+
+  it('Should throw on duplicated UID', () => {
+    const user2 = async () => {
+      try {
+        await signupModel.createUser(
+          'batman',
+          'batman@thebatcave.com',
+          'ba927d96-3b2d-11ee-be56-0242ac120002x',
+        );
+        fail('Expected createUser to throw an error');
+      } catch (error) {
+        throw new Error('Unable to create new user');
+      }
+    };
+    expect(user2).rejects.toThrow('Unable to create new user');
+  });
+
+  it('Should throw on duplicated username', () => {
+    const user2 = async () => {
+      try {
+        await signupModel.createUser(
+          'frogman',
+          'isafrog@thepond.com',
+          'ba916d96-3b2d-12ee-be256-0242ac188242x',
+        );
+        fail('Expected createUser to throw an error');
+      } catch (error) {
+        throw new Error('Unable to create new user');
+      }
+    };
+    expect(user2).rejects.toThrow('Unable to create new user');
+  });
+
+  it('Should throw on duplicated email', () => {
+    const user2 = async () => {
+      try {
+        await signupModel.createUser(
+          'froggy',
+          'frogman@test.com',
+          'ba987d96-37dd-14ee-be256-0922ac188362x',
+        );
+        fail('Expected createUser to throw an error');
+      } catch (error) {
+        throw new Error('Unable to create new user');
+      }
+    };
+    expect(user2).rejects.toThrow('Unable to create new user');
+  });
 });
