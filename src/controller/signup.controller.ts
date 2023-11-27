9a90688e8e4eb67736d6867908c3c1c7b87dd796
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-11-27 21:55:25 +0800

    Create additional checks for user creation
    feat: implement checks for empty inputs, valid uids, emails and usernames
    
    fix: Handle Prisma errors with custom messages
    
    Add try-catch block and async/await for improved error handling.
    
    fix: lint errors
    
    fix: improve user feedback by sending error msg
    
    chore: enable husky prepush hook
    
    fixup! Create additional checks for user creation

--- src/controller/signup.controller.ts
+++ src/controller/signup.controller.ts
@@ -8,6 +8,11 @@ export default {
       const id = await serviceSignup.createUser(req.body);
       res.status(201).send(id);
     } catch (error) {
+      let message;
+      if (error instanceof Error) {
+        message = error;
+        res.status(400).send(message.toString());
+      }
       res.status(400);
     }
   },
